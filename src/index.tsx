0f6d5fad7be77adc42cd33d4520b021506b9030d
Lucas Quadros <1lucascq@gmail.com>
2024-10-23 18:39:36 -0300

    feat: wrap app with theme and react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from '@mui/material/styles';
+
 import './index.css';
 import App from './App';
+import theme from './theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
