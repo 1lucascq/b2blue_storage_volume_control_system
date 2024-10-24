cd39eb86f33a3a17233a1ebcaaaf7ea13aa839a2
Lucas Quadros <1lucascq@gmail.com>
2024-10-24 09:11:40 -0300

    feat: add update stations option

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,29 @@
 import supabase from '../services/supabaseClient';
 import { Station } from '../ts/types';
 
-export const fetchItems = async (): Promise<Station[]> => {
-	const { data: stations, error } = await supabase.from<string, Station[]>('stations').select('*');
+export const fetchData = async (table: string): Promise<Station[]> => {
+    const { data: stations, error } = await supabase.from<string, Station[]>(table).select('*');
 
-	if (error) {
-		throw new Error(error.message);
-	}
-	return stations as Station[] || [];
+    if (error) {
+        throw new Error(error.message);
+    }
+    return (stations as Station[]).sort((a, b) => a.id - b.id) || [];
+};
+
+export const updateStations = async (
+    table: string,
+    updateFields: Partial<Station>,
+    id: number
+): Promise<Station[]> => {
+    const { data: station, error } = await supabase
+        .from(table)
+        .update(updateFields)
+        .eq('id', id)
+        .select();
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return (station as Station[]) || [];
 };
