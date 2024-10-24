30bfec80f68b672c24dab48d2fb659d35e5ab1b0
Lucas Quadros <1lucascq@gmail.com>
2024-10-24 09:12:16 -0300

    feat: format && improve component functionalities

--- src/components/StorageStation.tsx
+++ src/components/StorageStation.tsx
@@ -1,107 +1,178 @@
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, Button, Slide, Slider, Box } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Button, Slide, Slider, Box, Tooltip } from '@mui/material';
+import WarningIcon from '@mui/icons-material/Warning';
+
 import { StorageStationProps } from '../ts/types';
 
-const StorageStation: React.FC<StorageStationProps> = ({ stationName, volume, onCollect, onVolumeChange }) => {
-	const [isCollectionAvailable, setIsCollectionAvailable] = useState(false);
-	const [isCollectionRequested, setIsCollectionRequested] = useState(false);
-	const [currentVolume, setCurrentVolume] = useState(volume);
+const StorageStation: React.FC<StorageStationProps> = ({
+    stationName,
+    volume,
+    onCollect,
+    onVolumeChange,
+}) => {
+    const [isCollectionAvailable, setIsCollectionAvailable] = useState(false);
+    const [isCollectionRequested, setIsCollectionRequested] = useState(false);
+    const [currentVolume, setCurrentVolume] = useState(volume);
+    const collectionVolume = 80;
 
-	const handleSliderChange = (_event: any, newValue: number | number[]) => {
-		setCurrentVolume(newValue as number);
-	};
+    const handleSliderChange = (_event: any, newValue: number | number[]) => {
+        setCurrentVolume(newValue as number);
+    };
 
-	if (volume >= 80 && !isCollectionAvailable) setIsCollectionAvailable(true);
+    useEffect(() => {
+        if (volume >= collectionVolume && !isCollectionAvailable && currentVolume === volume) {
+            setIsCollectionAvailable(true);
+        } else {
+            setIsCollectionAvailable(false);
+        }
+    }, [volume, currentVolume]);
 
-	function handleCollect() {
-		setIsCollectionRequested(true);
-		console.log('pedido de coleta gerado');
-		setTimeout(() => {
-			onCollect();
-			setIsCollectionAvailable(false);
-			setIsCollectionRequested(false);
-		}, 10000);
-	}
+    function handleCollect() {
+        setIsCollectionRequested(true);
+        console.log('pedido de coleta gerado');
+        setTimeout(() => {
+            onCollect();
+            setIsCollectionAvailable(false);
+            setIsCollectionRequested(false);
+        }, 10000);
+    }
 
-	return (
-		<Card sx={{ boxShadow: 3, border: '1px solid', borderColor: 'primary.light', position: 'relative', padding: 4 }}>
-			<CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-				<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-					<Typography variant='h5'>{stationName}</Typography>
-					<Typography variant='body1' fontWeight='bold'>
-						{currentVolume}%
-					</Typography>
-				</Box>
+    function handleReset() {
+        setCurrentVolume(volume);
+    }
 
-				<Box sx={{ width: '100%' }}>
-					<Slider
-						value={currentVolume}
-						disabled={isCollectionRequested}
-						onChange={handleSliderChange}
-						aria-labelledby='continuous-slider'
-						valueLabelDisplay='auto'
-						min={0}
-						max={100}
-						size='medium'
-						sx={{
-							height: 18,
-							'& .MuiSlider-track': {
-								background: 'linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(121,90,9,1) 70%, rgba(255,0,57,1) 100%)',
-							},
-							'& .MuiSlider-thumb': {
-								backgroundColor: 'white',
-							},
-						}}
-					/>
-				</Box>
+    function displayValue() {
+        if (currentVolume === volume) {
+            return `${volume}%`;
+        }
 
-				<Box display='flex' justifyContent='space-between' width='100%' gap={2}>
-					<Button
-						fullWidth
-						variant='contained'
-						color='primary'
-						onClick={onVolumeChange.bind(null, currentVolume)}
-						sx={{
-							animation: currentVolume !== volume ? 'shake 3s infinite' : 'none',
-							'@keyframes shake': {
-								'0%': { transform: 'translate(0, 0)' },
-								'2%': { transform: 'translate(-2px, -2px)' },
-								'4%': { transform: 'translate(2px, 2px)' },
-								'6%': { transform: 'translate(2px, -2px)' },
-								'8%': { transform: 'translate(-2px, 2px)' },
-								'10%': { transform: 'translate(0, 0)' },
-								'100%': { transform: 'translate(0, 0)' },
-							},
-						}}
-					>
-						Atualizar
-					</Button>
-					<Button fullWidth variant='contained' color='primary' onClick={handleCollect}>
-						Redefinir
-					</Button>
-					<Button disabled fullWidth variant='contained' color='primary' onClick={handleCollect}>
-						Iniciar Coleta
-					</Button>
-				</Box>
-			</CardContent>
+        if (currentVolume > volume) {
+            return (
+                <>
+                    {volume}% &rarr;{' '}
+                    <Typography component="span" fontWeight="bold" color="#28a745">
+                        {currentVolume}%
+                    </Typography>
+                </>
+            );
+        }
 
-			<Slide direction='up' in={isCollectionRequested} mountOnEnter unmountOnExit timeout={{ enter: 500, exit: 500 }}>
-				<Box
-					sx={{
-						position: 'absolute',
-						bottom: 0,
-						width: '100%',
-						backgroundColor: 'tomato',
-						color: 'white',
-						textAlign: 'center',
-						padding: 1,
-					}}
-				>
-					Coleta em andamento
-				</Box>
-			</Slide>
-		</Card>
-	);
+        return (
+            <>
+                {volume}% &rarr;{' '}
+                <Typography component="span" fontWeight="bold" color="#dc3545">
+                    {currentVolume}%
+                </Typography>
+            </>
+        );
+    }
+    return (
+        <Card
+            sx={{
+                boxShadow: 3,
+                border: '1px solid',
+                borderColor: 'primary.light',
+                position: 'relative',
+                padding: 4,
+            }}
+        >
+            <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <Box
+                    sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                >
+                    <Typography variant="h5">{stationName}</Typography>
+                    <Typography variant="body1" fontWeight="bold">
+                        {displayValue()}
+                        {currentVolume !== volume && currentVolume >= collectionVolume && (
+                            <Tooltip
+                                title="Salve o novo volume para iniciar a coleta"
+                                placement="top"
+                            >
+                                <WarningIcon sx={{ color: 'orange', position: 'absolute' }} />
+                            </Tooltip>
+                        )}
+                    </Typography>
+                </Box>
+
+                <Box sx={{ width: '100%' }}>
+                    <Slider
+                        value={currentVolume}
+                        disabled={isCollectionRequested}
+                        onChange={handleSliderChange}
+                        aria-labelledby="continuous-slider"
+                        valueLabelDisplay="auto"
+                        min={0}
+                        max={100}
+                        size="medium"
+                        sx={{
+                            height: 18,
+                            '& .MuiSlider-track': {
+                                background:
+                                    'linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(121,90,9,1) 70%, rgba(255,0,57,1) 100%)',
+                            },
+                            '& .MuiSlider-thumb': {
+                                backgroundColor: 'white',
+                            },
+                        }}
+                    />
+                </Box>
+
+                <Box display="flex" justifyContent="space-between" width="100%" gap={2}>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={() => onVolumeChange(currentVolume)}
+                        sx={{
+                            animation:
+                                currentVolume !== volume ? 'colorChange 1s infinite' : 'none',
+                            '@keyframes colorChange': {
+                                '0%': { backgroundColor: 'primary.main' },
+                                '50%': { backgroundColor: 'primary.light' },
+                                '100%': { backgroundColor: 'primary.main' },
+                            },
+                        }}
+                    >
+                        Atualizar
+                    </Button>
+                    <Button fullWidth variant="contained" color="primary" onClick={handleReset}>
+                        Redefinir
+                    </Button>
+                    <Button
+                        disabled={!isCollectionAvailable}
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={handleCollect}
+                    >
+                        Confirmar Coleta
+                    </Button>
+                </Box>
+            </CardContent>
+
+            <Slide
+                direction="up"
+                in={isCollectionRequested}
+                mountOnEnter
+                unmountOnExit
+                timeout={{ enter: 500, exit: 500 }}
+            >
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        bottom: 0,
+                        width: '100%',
+                        backgroundColor: 'tomato',
+                        color: 'white',
+                        textAlign: 'center',
+                        padding: 1,
+                    }}
+                >
+                    Coleta em andamento
+                </Box>
+            </Slide>
+        </Card>
+    );
 };
 
 export default StorageStation;
