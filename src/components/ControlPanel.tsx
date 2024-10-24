2290393bdebe26d04aadbd12292f32979d125f10
Lucas Quadros <1lucascq@gmail.com>
2024-10-24 16:49:05 -0300

    feat: refactor functionalities and improve station management functions

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -2,19 +2,11 @@ import React, { useState, useEffect } from 'react';
 import StorageStation from './StorageStation';
 import { Box } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { fetchData, updateStations } from '../utils/utils';
-import { Station } from '../ts/types';
+import { fetchData, insertReport, updateStation } from '../utils/utils';
+import { ControlPanelProps, Station } from '../ts/types';
 
-const ControlPanel: React.FC = () => {
-    const {
-        data: stationsData,
-        error,
-        isLoading,
-    } = useQuery<Station[], Error>({
-        queryKey: ['stations'],
-        queryFn: () => fetchData('stations'),
-    });
-    const [stations, setStations] = useState<Station[]>([]);
+const ControlPanel: React.FC<ControlPanelProps> = ({ userName, stationsData }) => {
+    const [stations, setStations] = useState<Station[]>(stationsData);
 
     useEffect(() => {
         if (stationsData) {
@@ -24,30 +16,57 @@ const ControlPanel: React.FC = () => {
 
     // if (error) return <div>Error: {error.message}</div>;
 
-    if (isLoading) return <div>Loading...</div>;
-
-    const handleCollect = (index: number) => {
+    const handleStationUpdate = (index: number, updatedFields: Partial<Station>) => {
         const newStations = [...stations];
-        newStations[index].volume = 0;
+        newStations[index] = { ...newStations[index], ...updatedFields };
+        updateStation('stations', updatedFields, newStations[index].id);
         setStations(newStations);
+        console.log('handleStationUpdate --> ', stations);
     };
 
-    const handleVolumeChange = (index: number, newVolume: number) => {
-        const newStations = [...stations];
-        newStations[index].volume = newVolume;
-        updateStations('stations', { volume: newVolume }, newStations[index].id);
-        setStations(newStations);
+    const handleStartCollection = async (index: number) => {
+        const newValues = { collectionInProgress: true };
+        await updateStation('stations', newValues, stations[index].id);
+        handleStationUpdate(index, newValues);
+
+        // if (!updatedStation.collectionInProgress) throw new Error('Failed to update station');
     };
 
+    function getReportData(index: number) {
+        const date = new Date();
+        const gmt3Offset = 180;
+        const adjustedISODate = new Date(date.getTime() - gmt3Offset * 60 * 1000).toISOString();
+
+        return {
+            date: adjustedISODate,
+            station: stations[index].name,
+            registeredBy: userName,
+        };
+    }
+
+    const handleCompleteCollection = async (index: number): Promise<Station> => {
+        const newValues = { collectionInProgress: false, volume: 0 };
+        const completedStation = await updateStation('stations', newValues, stations[index].id);
+        const report = await insertReport('collectionReports', getReportData(index));
+        handleStationUpdate(index, newValues);
+
+        return completedStation;
+    };
+
+    console.log('rendering ControlPanel --> stations', stations);
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
             {stations.map((station, index) => (
                 <StorageStation
                     key={index}
                     stationName={station.name}
+                    collectionInProgress={station.collectionInProgress}
                     volume={station.volume}
-                    onCollect={() => handleCollect(index)}
-                    onVolumeChange={(newVolume) => handleVolumeChange(index, newVolume)}
+                    onStartCollection={() => handleStartCollection(index)}
+                    onStationChange={(updatedFields: Partial<Station>) =>
+                        handleStationUpdate(index, updatedFields)
+                    }
+                    onCompleteCollection={() => handleCompleteCollection(index)}
                 />
             ))}
         </Box>
