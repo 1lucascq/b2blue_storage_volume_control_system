00d3fd904bfed692e31842738e89e95ca87f0ef5
Lucas Quadros <1lucascq@gmail.com>
2024-10-24 16:47:15 -0300

    feat: refactor ControlPanel to receive station props

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,63 @@ import ControlPanel from './components/ControlPanel';
 import Header from './components/Header';
 import { Typography, Container } from '@mui/material';
 import NameModal from './components/NameModal';
+import { useQuery } from '@tanstack/react-query';
+import { Station } from './ts/types';
+import { fetchData } from './utils/utils';
 
 function App() {
-	const [isModalOpen, setIsModalOpen] = useState(true);
-	const [userName, setUserName] = useState('');
+    const {
+        data: stationsData,
+        error,
+        isLoading,
+    } = useQuery<Station[], Error>({
+        queryKey: ['stations'],
+        queryFn: () => fetchData('stations'),
+    });
 
-	const handleModalClose = (name: string) => {
-		localStorage.setItem('userName', name);
-		setUserName(name);
-		setIsModalOpen(false);
-	};
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [userName, setUserName] = useState('');
 
-	useEffect(() => {
-		const storedName = localStorage.getItem('userName');
+    useEffect(() => {
+        const storedName = localStorage.getItem('userName');
 
-		if (storedName) {
-			setUserName(storedName);
-			setIsModalOpen(false);
-		}
-	}, []);
+        if (storedName) {
+            setUserName(storedName);
+            setIsModalOpen(false);
+        }
+    }, []);
 
-	useEffect(() => {
-		if (userName) {
-			localStorage.setItem('userName', userName);
-		}
-	}, [userName]);
+    useEffect(() => {
+        if (userName) {
+            localStorage.setItem('userName', userName);
+        }
+    }, [userName]);
 
-	return (
-		<>
-			<Header />
-			<Typography variant='h3' component='h1' sx={{ flexGrow: 1, textAlign: 'center', my: 5 }}>
-				Sistema de Controle de Volume de Armazenamento
-			</Typography>
+    const handleModalClose = (name: string) => {
+        localStorage.setItem('userName', name);
+        setUserName(name);
+        setIsModalOpen(false);
+    };
 
-			<Container className='App' maxWidth='md'>
-				<ControlPanel />
-			</Container>
-			<NameModal open={isModalOpen} onClose={handleModalClose} />
-		</>
-	);
+    if (isLoading) return <div>Loading...</div>;
+
+    return (
+        <>
+            <Header />
+            <Typography
+                variant="h3"
+                component="h1"
+                sx={{ flexGrow: 1, textAlign: 'center', my: 5 }}
+            >
+                Sistema de Controle de Volume de Armazenamento
+            </Typography>
+
+            <Container className="App" maxWidth="md">
+                <ControlPanel userName={userName} stationsData={stationsData!} />
+            </Container>
+            <NameModal open={isModalOpen} onClose={handleModalClose} />
+        </>
+    );
 }
 
 export default App;
