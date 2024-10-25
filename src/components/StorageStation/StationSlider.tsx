60ae6dd7a8a9afc7943bc03fc117cf5e4a9ddc70
Lucas Quadros <1lucascq@gmail.com>
2024-10-25 17:29:34 -0300

    feat: update gradient from stations slider

--- src/components/StorageStation/StationSlider.tsx
+++ src/components/StorageStation/StationSlider.tsx
@@ -3,6 +3,30 @@ import { Box, Slider } from '@mui/material';
 import { StationSliderProps } from '../../ts/types';
 
 const StationSlider: React.FC<StationSliderProps> = ({ value, disabled, onChange }) => {
+    const getSliderColor = (value: number) => {
+        switch (true) {
+            case value < 50:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(0,212,255,1) 100%)';
+            case value >= 50 && value < 55:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 90%, rgba(255, 240, 23, 0.933) 100%)';
+            case value >= 55 && value < 60:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 83%, rgba(255, 240, 23, 0.933) 100%)';
+            case value >= 60 && value < 65:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 77%, rgba(255, 240, 23, 0.933) 100%)';
+            case value >= 65 && value < 70:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 67%, rgba(255, 240, 23, 0.933) 100%)';
+            case value >= 70 && value < 75:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 63%, rgba(255, 240, 23, 0.933) 97%, rgba(255,0,0,1) 100%)';
+            case value >= 75 && value < 80:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 55%, rgba(255, 240, 23, 0.933) 95%, rgba(255,0,0,1) 100%)';
+            case value >= 80 && value < 90:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 55%, rgba(255, 240, 23, 0.933) 93%, rgba(255,0,0,1) 100%)';
+            case value >= 90:
+                return 'linear-gradient(90deg, rgba(0,212,255,1) 50%, rgba(255, 240, 23, 0.933) 80%, rgba(255,0,0,1) 100%)';
+            default:
+                break;
+        }
+    };
     return (
         <Box sx={{ width: '100%' }}>
             <Slider
@@ -17,8 +41,7 @@ const StationSlider: React.FC<StationSliderProps> = ({ value, disabled, onChange
                 sx={{
                     height: 18,
                     '& .MuiSlider-track': {
-                        background:
-                            'linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(121,90,9,1) 70%, rgba(255,0,57,1) 100%)',
+                        background: getSliderColor(value),
                     },
                     '& .MuiSlider-thumb': {
                         backgroundColor: 'white',
