da6c6490a04c7b27a269c54a44b57870506f9a62
Lucas Quadros <1lucascq@gmail.com>
2024-10-25 10:15:31 -0300

    feat: add custom hooks to improve code readability and reusability

---
+++ src/utils/CustomHooks.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const useUserName = () => {
+    const [userName, setUserName] = useState<string>('');
+    const [isNameModalOpen, setIsNameModalOpen] = useState<boolean>(true);
+
+    useEffect(() => {
+        const storedName = localStorage.getItem('userName');
+
+        if (storedName) {
+            setUserName(storedName);
+            setIsNameModalOpen(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (userName) {
+            localStorage.setItem('userName', userName);
+        }
+    }, [userName]);
+
+    return { userName, setUserName, isNameModalOpen, setIsNameModalOpen };
+};
+
+export default useUserName;
